7a96b37b29ac19ad100f383fef7d77c23db46a56
gedeno <gedenogeremew@gmail.com>
2026-03-05 11:22:25 +0300

    small editions

--- main/views.py
+++ main/views.py
@@ -5,12 +5,14 @@ from django.views import View
 
 class Tasklist(View):
     def get(self,request):
-        tasks = Task.objects.all().order_by('-updated')
+        tasks = Task.objects.all()
         context = {'task':tasks}
         return render(request,'main/home.html',context)
     def post(self,request):
         task = Task.objects.create(
-            body = request.POST.get('body')
+            task_name = request.POST.get('task_name'),
+            task_description = request.POST.get('task_description'),
+            complited = request.POST.get('complited') == 'on'
         )
         task.save()
         return redirect('tasks')
