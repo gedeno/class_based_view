4ab04805d48de2483e78713617932284873d8b9a
gedeno <gedenogeremew@gmail.com>
2026-03-06 15:33:48 +0300

    small edtions

--- main/views.py
+++ main/views.py
@@ -1,19 +1,11 @@
 from django.shortcuts import render,redirect
 from .models import Task 
+from .forms import FormTask
 # Create your views here.
 from django.views import View
 from django.views.generic import CreateView
 
 class Tasklist(CreateView):
-    def get(self,request):
-        tasks = Task.objects.all()
-        context = {'task':tasks}
-        return render(request,'main/home.html',context)
-    def post(self,request):
-        task = Task.objects.create(
-            task_name = request.POST.get('task_name'),
-            task_description = request.POST.get('task_description'),
-            completed = request.POST.get('completed') == 'on'
-        )
-        task.save()
-        return redirect('tasks')
+    model = Task
+    form_class = FormTask
+    template_name = "main/home.html"
