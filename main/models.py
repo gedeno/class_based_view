7fd69142c62857da6d7aea9e652f5d5bd40b4034
gedeno <gedenogeremew@gmail.com>
2026-03-06 19:30:15 +0300

    small editions

--- main/models.py
+++ main/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     task_name = models.CharField(max_length=300)
-    task_description = models.TextField(max_length=300)
+    task_description = models.CharField(max_length=300)
     completed = models.BooleanField(default=False)
